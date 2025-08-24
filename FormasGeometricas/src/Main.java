79ed689e9a153d68416aa85fef9c45a0a9087ab7
Guilherme <gui_parracho@hotmail.com>
2025-08-23 22:38:14 -0300

    Parte 2: Formas Geométricas

---
+++ FormasGeometricas/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Quadrado quadrado = new Quadrado(4);
+        Circulo circulo = new Circulo(3);
+
+        Desenho desenho = new Desenho();
+        desenho.formas = List.of(quadrado, circulo);
+        System.out.printf("Total Quadrado + Circulo: %.2f \n", desenho.calcularAreaTotal());
+        System.out.printf("Área do quadrado: %.2f \n", quadrado.calcularArea());
+        System.out.printf("Área do círculo: %.2f ", circulo.calcularArea());
+    }
+}
